ca300dfb6aa57b3a319abf11aaa03ae2128c594a
Jose Angel Leyva <joseangel.leyva@outlook.com>
2021-10-22 15:13:31 -0500

    Corregir identificacion de S3

--- FileUpload/lib/FileUpload.pm
+++ FileUpload/lib/FileUpload.pm
@@ -17,11 +17,11 @@ post '/enviar' => sub {
     my @archivos = request->upload('archivo');
 
     foreach my $archivo (@archivos) {
-            my $s3 = Amazon::S3->new(
+            my $s3 = Amazon::S3->new({
                     aws_secret_key_id => 'AKIAWX5GLZKAQIFDZJCM',
                     aws_secret_access_key => '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',
                     retry => 1
-            );
+            });
 
             my $bucket =  $s3->bucket('hackathon-test-bbva-2021');
             $bucket->add_key(time, $archivo->content, { content_type => 'text/plain'});
