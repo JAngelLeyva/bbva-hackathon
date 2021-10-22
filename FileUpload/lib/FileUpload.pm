4aab96dd80e0ca6fb6380caed97962c832505986
Jose Angel Leyva <joseangel.leyva@outlook.com>
2021-10-22 15:02:13 -0500

    Carga en S3

--- FileUpload/lib/FileUpload.pm
+++ FileUpload/lib/FileUpload.pm
@@ -1,6 +1,8 @@
 package FileUpload;
 use Dancer ':syntax';
 
+use Amazon::S3;
+
 our $VERSION = '0.1';
 
 get '/' => sub {
@@ -15,7 +17,14 @@ post '/enviar' => sub {
     my @archivos = request->upload('archivo');
 
     foreach my $archivo (@archivos) {
-            $archivo->copy_to(time . '.raw');
+            my $s3 = Amazon::S3->new(
+                    aws_secret_key_id => 'AKIAWX5GLZKAQIFDZJCM',
+                    aws_secret_access_key => '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',
+                    retry => 1
+            );
+
+            my $bucket =  $s3->bucket('hackathon-test-bbva-2021');
+            $bucket->add_key(time, $archivo->content, { content_type => 'text/plain');
     }
 
     return template 'enviar';
